a8a3c4ff011850d6ff0362f31e7d276ce604114c
mertkeserprac <mert.keser12@gmail.com>
2018-11-16 18:34:02 +0100

    Index attached to the File

---
+++ 1_OOP_Python/2_OOP_Exercises.py
@@ -0,0 +1,34 @@
+#This is an exercise for learning OOP in Python.
+
+class Dog:
+
+    #Class Attribute
+    species = 'mammal'
+
+
+    #Initializer/Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    #instance Method
+    def description(self):
+        return "{} is {} years old".format(self.name, self.age)
+
+    #instance method
+    def speak(self, sound):
+        return "{} says {}".format(self.name, sound)
+
+#Instantianete Dog Object
+philo = Dog('Philo', 5)
+mikey = Dog('Mikey', 6)
+
+#Access the instance attributes
+print("{} is {} and {} is {}.".format(philo.name, philo.age, mikey.name, mikey.age ))
+
+#Is Philo a mammal?
+if philo.species == "mammal":
+    print("{} is a {}!".format(philo.name, philo.species))
+
+print(philo.description())
+print(philo.speak('bark'))
